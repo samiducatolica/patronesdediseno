ceb1fd9d7c6d28812fe1a363acfdd96524a4832c
Luisa Paez <lfpaez30@ucatolica.edu.co>
2024-04-03 20:47:47 -0500

    Patron Observer

--- src/PatronObserver/Observer.java
+++ src/PatronObserver/Observer.java
@@ -1,4 +1,4 @@
-package PatonObserver;/*
+package PatronObserver;/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
